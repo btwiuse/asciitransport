84c2f7506392814eaea4a5ff80ff44f283b759e6
navigaid <navigaid@gmail.com>
2019-12-06 23:53:06 +0800

    update raw.go: support exit by pressing ESC twice

--- raw.go
+++ raw.go
@@ -4,23 +4,29 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"log"
 
 	"github.com/containerd/console"
 )
 
 func main() {
-	fmt.Println("vim-go")
+	fmt.Println("Press ESC twice to exit.")
 	current := console.Current()
 	defer current.Reset()
 
 	if err := current.SetRaw(); err != nil {
 		panic(err)
 	}
-	ws, err := current.Size()
-	if err != nil {
-		panic(err)
+	for buf := make([]byte, 4096); ; {
+		n, err := current.Read(buf)
+		if err != nil {
+			panic(err)
+		}
+		str := fmt.Sprintf("%q", string(buf[:n]))
+		log.Println(str)
+		if str == `"\x1b\x1b"` {
+			log.Println("BYE")
+			break
+		}
 	}
-	current.Resize(ws)
-	time.Sleep(time.Hour)
 }
